ab728e916d1abca23fc6d8d43911f1336f98cc67
Teddy-Suh <t3ddy.suh@gmail.com>
2024-09-30 11:54:44 +0900

    Fix: 스타일링 툴 관련 설정 변경
    theme을 추가하였고 postcss.confing.mjs에 autoprefixer를 추가 하였습니다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
 
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: ["light"],
+  },
 };
 export default config;
