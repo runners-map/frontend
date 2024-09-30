dc260c5a1ce92591af08ea148892b8a6ad990b70
Teddy-Suh <t3ddy.suh@gmail.com>
2024-09-30 19:20:35 +0900

    Feat : Register 페이지 구현
    Register/page.tsx와 RegisterFrom.tsx 작성했습니다. 아직 POST하는 부분은 구현하지 않은 초안입니다.

---
+++ src/app/register/RegisterForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { HiUser } from "react-icons/hi";
+import { PiGenderIntersexBold } from "react-icons/pi";
+
+const RegisterForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      nickname: "",
+      gender: "",
+    },
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label className="input input-bordered input-primary flex items-center gap-2">
+          <Controller
+            name="nickname"
+            control={control}
+            rules={{
+              required: "닉네임을 입력해 주세요.",
+              maxLength: {
+                value: 10,
+                message: "닉네임은 최대 10자까지 가능합니다.",
+              },
+            }}
+            render={({ field }) => (
+              <>
+                <HiUser className="opacity-70" />
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="닉네임"
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </label>
+        {errors.nickname && (
+          <span className="text-red-500">{errors.nickname.message}</span>
+        )}
+      </div>
+
+      <div>
+        <label className="flex">
+          <PiGenderIntersexBold className="opacity-70" />
+          <span className="opacity-70">성별</span>
+        </label>
+        <div className="flex space-x-4">
+          <Controller
+            name="gender"
+            control={control}
+            rules={{ required: "성별을 선택해 주세요." }}
+            render={({ field }) => (
+              <>
+                <label className="label cursor-pointer">
+                  <input
+                    type="radio"
+                    className="radio radio-primary"
+                    value="M"
+                    checked={field.value === "M"}
+                    onChange={() => field.onChange("M")}
+                  />
+                  <span className="ml-2">남성</span>
+                </label>
+                <label className="label cursor-pointer">
+                  <input
+                    type="radio"
+                    className="radio radio-primary"
+                    value="F"
+                    checked={field.value === "F"}
+                    onChange={() => field.onChange("F")}
+                  />
+                  <span className="ml-2">여성</span>
+                </label>
+              </>
+            )}
+          />
+        </div>
+        {errors.gender && (
+          <span className="text-red-500">{errors.gender.message}</span>
+        )}
+      </div>
+
+      <button type="submit" className="btn btn-outline btn-primary">
+        가입하기
+      </button>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,18 @@
+import RegisterForm from "./RegisterForm";
+
+export const metadata = {
+  title: "회원가입",
+};
+
 export default function RegisterPage() {
-  return <div>정보 입력 페이지</div>;
+  return (
+    <>
+      <h1>
+        Runners&apos; Map에 오신걸 환영합니다.
+        <br />
+        아래 정보를 기입해 주세요.
+      </h1>
+      <RegisterForm />
+    </>
+  );
 }
