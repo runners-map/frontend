4755ccb44a44a08cc3d9d6bb32e11a9bf8fc908d
Teddy-Suh <t3ddy.suh@gmail.com>
2024-09-30 20:07:22 +0900

    Style : 네비게이션 스타일링
    하단 네비게이션을 스타일링 하였습니다.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,64 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  HiOutlineMap,
+  HiOutlineChatBubbleOvalLeftEllipsis,
+  HiOutlineChartBar,
+  HiOutlineTrophy,
+  HiOutlineUser,
+} from "react-icons/hi2";
 
 export default function Navigation() {
+  const currentPath = usePathname();
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link href={"/"}>맵</Link>
-          </li>
-          <li>
-            <Link href={"/chat-list"}>채팅방 목록</Link>
-          </li>
-          <li>
-            <Link href={"/chart"}>기록</Link>
-          </li>
-          <li>
-            <Link href={"/ranking"}>랭킹</Link>
-          </li>
-          <li>
-            <Link href={"/mypage"}>마이페이지</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav className="btm-nav max-w-md mx-auto">
+      <Link
+        href={"/"}
+        className={`text-primary ${
+          currentPath === "/" ? "active" : "text-gray-500"
+        }`}
+      >
+        <HiOutlineMap size={25} />
+        <span className="btm-nav-label">맵</span>
+      </Link>
+      <Link
+        href={"/chat-list"}
+        className={`text-primary ${
+          currentPath === "/chat-list" ? "active" : "text-gray-500"
+        }`}
+      >
+        <HiOutlineChatBubbleOvalLeftEllipsis size={25} />
+        <span className="btm-nav-label">채팅</span>
+      </Link>
+      <Link
+        href={"/ranking"}
+        className={`text-primary ${
+          currentPath === "/ranking" ? "active" : "text-gray-500"
+        }`}
+      >
+        <HiOutlineTrophy size={25} />
+        <span className="btm-nav-label">랭킹</span>
+      </Link>
+      <Link
+        href={"/chart"}
+        className={`text-primary ${
+          currentPath === "/chart" ? "active" : "text-gray-500"
+        }`}
+      >
+        <HiOutlineChartBar size={25} />
+        <span className="btm-nav-label">기록</span>
+      </Link>
+      <Link
+        href={"/mypage"}
+        className={`text-primary ${
+          currentPath === "/mypage" ? "active" : "text-gray-500"
+        }`}
+      >
+        <HiOutlineUser size={25} />
+        <span className="btm-nav-label">마이 페이지</span>
+      </Link>
+    </nav>
   );
 }
