6ea4976fe3aa67f4e596966a935342be2c1e6ad2
chanjeong park <ckswjd9595@gmail.com>
2024-10-02 18:24:07 +0900

    Feat:채팅방목록페이지 및 로딩페이지 생성, 약간의 db 수정, chat-list/id/post-info 페이지도 약간의 기능 추가

--- src/app/chat-list/page.tsx
+++ src/app/chat-list/page.tsx
@@ -1,22 +1,29 @@
 import { ChatRoom } from '@/types/ChatRoom';
 import axios from 'axios';
+import Link from 'next/link';
 
 export const metadata = {
   title: '채팅방 목록'
 };
 
 const getChatList = async () => {
+  await new Promise(resolve => setTimeout(resolve, 5000));
   const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/ChatRoom`);
   return response.data;
 };
 
 export default async function ChatListPage() {
   const chats = await getChatList();
-
   return (
-    <ul>
+    <ul className="p-14">
       {chats.map((chat: ChatRoom) => (
-        <li key={chat.chat_room_id}>{chat.title}</li>
+        <li
+          key={chat.chat_room_id}
+          className="bg-gradient-to-r from-blue-200 to-purple-200 shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300 mb-5">
+          <Link href={`/chat-list/${chat.chat_room_id}/post-info`} className="block p-6">
+            <h2 className="text-xl font-semibold text-gray-900 mb-4">{chat.title}</h2>
+          </Link>
+        </li>
       ))}
     </ul>
   );
