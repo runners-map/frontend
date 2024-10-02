81de1e1a43d386c591ab8d901111b073f2804713
Teddy-Suh <t3ddy.suh@gmail.com>
2024-10-02 12:13:50 +0900

    Fix: 신규 회원 정보 기입 페이지 충돌 해결

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import RegisterForm from "./RegisterForm";
 
 export const metadata = {
@@ -17,23 +16,3 @@ export default function RegisterPage() {
     </>
   );
 }
-=======
-// 서버 컴포넌트
-import RegisterForm from "./RegisterForm";
-
-export const metadata = {
-  title: "회원가입 페이지",
-  description: "새로운 사용자를 위한 회원가입 페이지입니다.",
-};
-
-const RegisterPage = () => {
-  return (
-    <div>
-      <h1>Runners&apos; Map에 오신걸 환영합니다.</h1>
-      <RegisterForm />
-    </div>
-  );
-};
-
-export default RegisterPage;
->>>>>>> Stashed changes
