b6a0928239ecb3f4243765771391c9eea0f0666d
chanjeong park <ckswjd9595@gmail.com>
2024-10-04 19:00:20 +0900

    Feat: 모집글 작성 페이지에서 시간선택ui보완
    Feat: types에서 모집글 조회를 위한 타입 설정
    
    Feat: 맵페이지에서 모달을 띄워 모집글 조회 모달 구현

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,10 +1,40 @@
 export interface Post {
-  gender: string;
-  limitMemberCount: number;
-  paceKm: number;
-  paceTime: string;
-  distance: string;
-  startDateTime: Date | null;
+  postId: number;
+  adminId: string;
   title: string;
   content: string;
+  limitMemberCnt: number;
+  gender: string;
+  startDateTime: Date;
+  startPosition: Date;
+  distance: number;
+  paceMin: number;
+  paceSec: number;
+  path: string;
+  departureYn: boolean;
+  arriveYn: boolean;
+  lat: number;
+  lng: number;
+  createdDateTime: string;
+  updatedDateTime: string;
+}
+
+export interface GetPostsRequest {
+  swLatlng: string;
+  neLatlng: string;
+  gender: string;
+  paceMinStart: number;
+  paceMinEnd: number;
+  distanceStart: number;
+  distanceEnd: number;
+  startDate: string;
+  startTime: string;
+  limitMemberCnt: number;
+}
+
+export interface GetPostsResponse {
+  status: number;
+  errorCode?: string;
+  message?: string;
+  data: Post[];
 }
