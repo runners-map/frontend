e0137a190e9ecd7a2b6ccfef8d299f31c66c2c10
Teddy-Suh <t3ddy.suh@gmail.com>
2024-10-10 12:14:36 +0900

    feat : 마이페이지 초안

--- src/app/mypage/MypageButtons.tsx
+++ src/app/mypage/MypageButtons.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-
+import {
+  HiOutlineCog6Tooth,
+  HiMiniArrowRightOnRectangle,
+  HiOutlineUserMinus,
+} from "react-icons/hi2";
 export default function MyPageButtons() {
   const router = useRouter();
 
@@ -14,10 +18,16 @@ export default function MyPageButtons() {
         className="btn btn-outline btn-primary w-full"
         onClick={handleClickSettingBtn}
       >
-        설정 변경
+        <HiOutlineCog6Tooth />내 정보 변경
       </button>
-      <button className="btn btn-outline btn-primary w-full">로그 아웃</button>
-      <button className="btn btn-outline btn-primary w-full">회원 탈퇴</button>
+      <button className="btn btn-outline btn-primary w-full">
+        <HiMiniArrowRightOnRectangle />
+        로그아웃
+      </button>
+      <button className="btn btn-outline btn-secondary w-full">
+        <HiOutlineUserMinus />
+        회원 탈퇴
+      </button>
     </>
   );
 }
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,13 +1,38 @@
+"use client";
 import MyPageButtons from "@/app/mypage/MypageButtons";
+import { useQuery } from "@tanstack/react-query";
+
+import axios from "axios";
+
+const fetchUserData = async () => {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+    params: {
+      userId: 1,
+    },
+  });
+  console.log(response.data);
+  return response.data;
+};
 
 export default function MyPagePage() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUserData,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="p-8">
-      <h1>마이 페이지</h1>
+      <h1 className="text-2xl font-bold mb-8 text-center">마이 페이지</h1>
       <div className="card border-2 border-primary">
         <div className="card-body">
-          <span>안녕하세요 </span>
-          <div className="card-actions">
+          <span className="text-center font-bold text-xl">{data.nickname}</span>
+          <span className="text-center">{data.email}</span>
+          <span className="text-center">리마인드 알림 30분</span>
+          <div className="card-actions mt-8 space-y-2">
             <MyPageButtons />
           </div>
         </div>
