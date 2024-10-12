c6e958be64ecce1ca85817a0ae51b48f9bebe3a1
Teddy-Suh <t3ddy.suh@gmail.com>
2024-10-12 12:22:07 +0900

    feat : 마이페이지 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["i.namu.wiki"], // 외부 이미지 도메인 추가
+  },
+};
 
 export default nextConfig;
