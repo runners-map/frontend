3922212efc7c20db6e2bf71545554b0d6efec4ea
chanjeong park <ckswjd9595@gmail.com>
2024-10-24 12:49:59 +0900

    Fix:충돌해결

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -28,9 +28,9 @@ export default function LoginForm() {
     try {
       const result = await fetchCall<{
         accessToken: string;
-        refreshToken: string;
+        refreshToken: string; 
         userInfo: UserInfoType;
-      }>('user/loginin', 'post', { email, password });
+      }>("user/loginin", "post", { email, password });
 
       const { accessToken, refreshToken, userInfo } = result;
 
