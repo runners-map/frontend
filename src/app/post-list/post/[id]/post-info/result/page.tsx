1dd6a356744cfc9716a04c41cec6f61e207cfa38
Teddy-Suh <t3ddy.suh@gmail.com>
2024-11-01 21:23:46 +0900

    fix: 오류 수정

--- src/app/post-list/post/[id]/post-info/result/page.tsx
+++ src/app/post-list/post/[id]/post-info/result/page.tsx
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-import Result from './Result';
+import Result from "./Result";
 
 export const metadata = {
-  title: '러닝 결과'
+  title: "러닝 결과",
 };
 
-export default function ResultPage({ params: { id } }: { params: { id: string } }) {
+export default function ResultPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   return <Result id={id} />;
 }
-=======
-import Result from './Result';
-
-export const metadata = {
-  title: '러닝 결과'
-};
-
-export default function ResultPage({ params: { id } }: { params: { id: string } }) {
-  return <Result id={id} />;
-}
->>>>>>> c3cc66b (fix : 충돌해결)
