570e047efed62c37f4597de7e766fe16b80bc007
chanjeong park <ckswjd9595@gmail.com>
2024-11-02 17:59:51 +0900

    Fix:충돌해결

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-<<<<<<< HEAD
 import { LoginFormData } from '@/types/LoginForm';
 import { UserInfoType, useUserInfo } from '@/types/UserInfo';
 import { useRouter } from 'next/navigation';
@@ -8,12 +7,6 @@ import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { HiMiniEnvelope, HiLockClosed } from 'react-icons/hi2';
 import Cookies from 'js-cookie';
 import axios from 'axios';
-=======
-import { LoginFormData } from "@/types/LoginForm";
-import { useRouter } from "next/navigation";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import { HiMiniEnvelope, HiLockClosed } from "react-icons/hi2";
->>>>>>> c3cc66b (fix : 충돌해결)
 
 export default function LoginForm() {
   const {
@@ -21,13 +14,11 @@ export default function LoginForm() {
     handleSubmit,
     formState: { errors }
   } = useForm<LoginFormData>({
-    mode: "onChange", // onChange 이벤트로 유효성 검사 설정
     defaultValues: {
       email: '',
       password: ''
     }
   });
-<<<<<<< HEAD
   const { saveUser, checkLogin, setUserId } = useUserInfo();
   const router = useRouter();
 
@@ -53,28 +44,17 @@ export default function LoginForm() {
     } catch (error) {
       console.log('로그인 실패', error);
     }
-=======
-
-  const router = useRouter();
-
-  const onSubmit: SubmitHandler<LoginFormData> = async (data) => {
-    router.push("/");
->>>>>>> c3cc66b (fix : 충돌해결)
   };
-
   const handleResister = () => {
     router.push('/register');
   };
 
   return (
-    <div className="shadow-md shadow-slate-300 rounded-2xl px-4 py-20 bg-white">
-      <h2 className="text-3xl font-bold mb-8 text-center text-primary">
-        Runner&apos;s Map
-      </h2>
+    <div className="card border-2 border-primary">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-8">
+        <div className="card-body space-y-6">
           <div>
-            <label className="input input-bordered input-primary flex items-center gap-2 mb-1 rounded-full">
+            <label className="input input-bordered input-primary flex items-center gap-2 mb-1">
               <Controller
                 name="email"
                 control={control}
@@ -88,17 +68,7 @@ export default function LoginForm() {
                 render={({ field }) => (
                   <>
                     <HiMiniEnvelope className="opacity-70" size={20} />
-<<<<<<< HEAD
                     <input type="email" className="grow" placeholder="이메일" {...field} />
-=======
-                    <input
-                      type="email"
-                      autoComplete="off"
-                      className="grow"
-                      placeholder="이메일"
-                      {...field}
-                    />
->>>>>>> c3cc66b (fix : 충돌해결)
                   </>
                 )}
               />
@@ -106,7 +76,7 @@ export default function LoginForm() {
             {errors.email && <span className="text-red-500 absolute">{errors.email.message}</span>}
           </div>
           <div>
-            <label className="input input-bordered input-primary flex items-center gap-2 mb-1 rounded-full">
+            <label className="input input-bordered input-primary flex items-center gap-2 mb-1">
               <Controller
                 name="password"
                 control={control}
@@ -114,59 +84,24 @@ export default function LoginForm() {
                   required: '비밀번호를 입력해 주세요.',
                   minLength: {
                     value: 8,
-<<<<<<< HEAD
                     message: '비밀번호는 최소 8자 이상이어야 합니다.'
                   }
-=======
-                    message: "비밀번호는 최소 8자 이상이어야 합니다.",
-                  },
-                  pattern: {
-                    value:
-                      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
-                    message: "영어, 숫자, 특수문자를 모두 포함해야 합니다.",
-                  },
->>>>>>> c3cc66b (fix : 충돌해결)
                 }}
                 render={({ field }) => (
                   <>
                     <HiLockClosed className="opacity-70" size={20} />
-<<<<<<< HEAD
                     <input type="password" className="grow" placeholder="비밀번호" {...field} />
-=======
-                    <input
-                      type="password"
-                      autoComplete="off"
-                      className="grow"
-                      placeholder="비밀번호"
-                      {...field}
-                    />
->>>>>>> c3cc66b (fix : 충돌해결)
                   </>
                 )}
               />
             </label>
             {errors.password && <span className="text-red-500 absolute">{errors.password.message}</span>}
           </div>
-<<<<<<< HEAD
           <div className="card-actions">
             <button type="submit" className="btn btn-primary w-full mb-2 text-base text-white">
               로그인
             </button>
             <button type="button" onClick={handleResister} className="btn btn-primary w-full text-base text-white">
-=======
-          <div className="card-actions pt-8">
-            <button
-              type="submit"
-              className="btn btn-primary w-full mb-2 text-base text-white rounded-full shadow-md shadow-slate-300"
-            >
-              로그인
-            </button>
-            <button
-              type="button"
-              onClick={handleResister}
-              className="btn btn-primary w-full text-base text-white rounded-full  shadow-md shadow-slate-300"
-            >
->>>>>>> c3cc66b (fix : 충돌해결)
               회원가입
             </button>
           </div>
--- src/app/post-list/post/[id]/post-info/result/page.tsx
+++ src/app/post-list/post/[id]/post-info/result/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Result from './Result';
 
 export const metadata = {
@@ -8,14 +7,3 @@ export const metadata = {
 export default function ResultPage({ params: { id } }: { params: { id: string } }) {
   return <Result id={id} />;
 }
-=======
-import Result from './Result';
-
-export const metadata = {
-  title: '러닝 결과'
-};
-
-export default function ResultPage({ params: { id } }: { params: { id: string } }) {
-  return <Result id={id} />;
-}
->>>>>>> c3cc66b (fix : 충돌해결)
